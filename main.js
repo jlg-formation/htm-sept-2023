d47618894004dd9eb989dbc5bf50cf0523c8b87d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-09-04 13:54:06 +0200

    dev: added points on circle

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
+const SVG_NS = "http://www.w3.org/2000/svg";
 const r = 1;
+const samples = 10;
+
+const cx0 = 50;
+const cy0 = 50;
+const r0 = 45;
 
 const gSamplesElt = document.querySelector("svg g.samples");
 console.log("gSamplesElt: ", gSamplesElt);
 
-const circleElt = document.createElementNS(
-  "http://www.w3.org/2000/svg",
-  "circle"
-);
-console.log("circleElt: %O", circleElt);
+for (let i = 0; i < samples; i++) {
+  const circleElt = document.createElementNS(SVG_NS, "circle");
+  console.log("circleElt: %O", circleElt);
 
-circleElt.setAttribute("cx", "34");
-circleElt.setAttribute("cy", "23");
-circleElt.setAttribute("r", r);
+  const angle = (i * (2 * Math.PI)) / samples;
+  const cx = cx0 + r0 * Math.cos(angle);
+  const cy = cy0 + r0 * Math.sin(angle);
 
-gSamplesElt.appendChild(circleElt);
+  circleElt.setAttribute("cx", cx);
+  circleElt.setAttribute("cy", cy);
+  circleElt.setAttribute("r", r);
+
+  gSamplesElt.appendChild(circleElt);
+}
